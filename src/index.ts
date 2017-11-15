fa10805782730bb50497923a08bce15d8818c1aa
luixaviles <luis.avilesz@gmail.com>
2017-11-15 01:45:51 -0400

    Adding cors support

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import 'reflect-metadata'; 
+import 'reflect-metadata';
 import { createExpressServer } from 'routing-controllers';
 import { SpeakerController } from './controllers/speaker.controller';
 
 const app = createExpressServer({
-   controllers: [SpeakerController]
+    cors: true,
+    controllers: [SpeakerController]
 });
 
 const PORT = 3000;
-
 app.listen(PORT);
 console.log('Running local server on port ', PORT);
