a756c7d1d920ef78cc394d37e99ad7475b5abd06
luixaviles <luis.avilesz@gmail.com>
2017-11-15 12:37:24 -0400

    Adding clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+const del = require('del');
 var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("build", function () {
+gulp.task("build", ['clean'], function () {
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest("./dist"));
+});
+
+gulp.task('clean', function(){
+    return del(['dist/**', '!dist'], {
+        force:true
+    });
 });
